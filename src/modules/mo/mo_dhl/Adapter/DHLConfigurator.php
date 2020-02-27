45afa2375cfd464bf3022c59467054648f224e71
mbe <martin.beckmann@mediaopt.de>
2020-02-27 09:27:30 +0100

    #12873 use old mediaopt credentials for rest calls

--- src/modules/mo/mo_dhl/Adapter/DHLConfigurator.php
+++ src/modules/mo/mo_dhl/Adapter/DHLConfigurator.php
@@ -35,7 +35,7 @@ class DHLConfigurator extends \Mediaopt\DHL\Configurator
      */
     protected function getRestLogin()
     {
-        return 'DHL_Oxid_1';
+        return 'DhlEmpfaengerservicesOxid_3';
     }
 
     /**
@@ -43,7 +43,7 @@ class DHLConfigurator extends \Mediaopt\DHL\Configurator
      */
     protected function getRestPassword()
     {
-        return 'J7sC6PNrnyAaKs7AifHBhZW51rLGjz';
+        return 'sLS0vunhKg47u6JyVTL0ZUDCG18Mh8';
     }
 
     /**
