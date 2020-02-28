c33635e5c0c10578132dd2ec7fafdf315247fa65
mbe <martin.beckmann@mediaopt.de>
2020-02-28 08:32:49 +0100

    Redo "#12873 use new oxid dhl appId"
    This reverts commit c3b11945

--- tests/TestConfigurator.php
+++ tests/TestConfigurator.php
@@ -4,12 +4,12 @@ class TestConfigurator extends \Mediaopt\DHL\Configurator
 {
     protected function getRestLogin()
     {
-        return 'DhlEmpfaengerservicesOxid_3';
+        return 'DHL_Oxid_1';
     }
 
     protected function getRestPassword()
     {
-        return 'sLS0vunhKg47u6JyVTL0ZUDCG18Mh8';
+        return 'J7sC6PNrnyAaKs7AifHBhZW51rLGjz';
     }
 
     protected function getSoapLogin()
