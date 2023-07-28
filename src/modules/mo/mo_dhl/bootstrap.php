563344e40a507f5afc5b54eaef1338c0aa01c786
realnaliboh <realnaliboh@gmail.com>
2023-07-28 16:01:41 +0200

    removed redudant bootstrap

--- src/modules/mo/mo_dhl/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if (file_exists(OX_BASE_PATH . '/overridablefunctions.php')) {
-    require_once OX_BASE_PATH . '/overridablefunctions.php';
-}
-require_once __DIR__ . '/vendor/autoload.php';
