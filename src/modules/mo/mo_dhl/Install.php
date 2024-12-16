819c4e2222c45b11938eccdc5ecfe7c9b5403fa1
mbe <martin.beckmann@mediaopt.de>
2024-12-16 09:45:06 +0100

    #37231 removed bootstrap.php

--- src/modules/mo/mo_dhl/Install.php
+++ src/modules/mo/mo_dhl/Install.php
@@ -23,7 +23,6 @@ class Install
      */
     public static function onActivate()
     {
-        static::addBootstrapLoader();
         static::ensureConfigVariableNameLength();
         static::addTables();
         static::addColumns();
@@ -41,44 +40,6 @@ class Install
         if (!is_dir($path)) {
             Registry::getUtilsView()->addErrorToDisplay(sprintf(Registry::getLang()->translateString('MO_DHL__INSTALL_FOLDER_ERROR'), $path));
         }
-    }
-
-    /**
-     * Adds a line to the functions.php in the modules directory to ensure that the bootstrap is included.
-     */
-    protected static function addBootstrapLoader()
-    {
-        $lines = file_exists(static::getFunctionsFile()) ? file(static::getFunctionsFile(), FILE_IGNORE_NEW_LINES) : ['<?php'];
-        $bootstrapLoader = static::getBootstrapLoaderStatement();
-
-        if (in_array($bootstrapLoader, $lines, true)) {
-            return;
-        }
-
-        $lineOfClosingTag = array_search('?>', $lines, true);
-        if ($lineOfClosingTag !== false) {
-            $lines = array_slice($lines, 0, $lineOfClosingTag);
-        }
-        $lines[] = $bootstrapLoader;
-
-        file_put_contents(static::getFunctionsFile(), implode(PHP_EOL, $lines) . PHP_EOL);
-    }
-
-    /**
-     * @return string
-     */
-    protected static function getFunctionsFile()
-    {
-        return OX_BASE_PATH . 'modules/functions.php';
-    }
-
-    /**
-     * @return string
-     */
-    protected static function getBootstrapLoaderStatement()
-    {
-        $comment = 'This line was automatically generated.';
-        return "require_once __DIR__ . '/mo/mo_dhl/bootstrap.php'; // {$comment}";
     }
 
     /**
@@ -134,27 +95,9 @@ class Install
      */
     public static function onDeactivate()
     {
-        static::removeBootstrapLoader();
         static::cleanUp();
     }
 
-    /**
-     * Removes the line in the functions.php in the modules directory that loads the bootstrap, in case this line
-     * exists.
-     */
-    protected static function removeBootstrapLoader()
-    {
-        if (!file_exists(static::getFunctionsFile())) {
-            return;
-        }
-
-        $lines = file(static::getFunctionsFile(), FILE_IGNORE_NEW_LINES);
-        $lineOfBootstrapLoader = array_search(static::getBootstrapLoaderStatement(), $lines, true);
-        if ($lineOfBootstrapLoader !== false) {
-            unset($lines[$lineOfBootstrapLoader]);
-        }
-        file_put_contents(static::getFunctionsFile(), implode(PHP_EOL, $lines) . PHP_EOL);
-    }
 
     /**
      * @throws \Exception
